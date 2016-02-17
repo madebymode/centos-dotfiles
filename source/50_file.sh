fd12b26218f9a3731f2d6661e8617f32c3bfe40a
Troy McCall <troyxmccall@gmail.com>
2016-02-17 11:57:48 -0500

    update

--- source/50_file.sh
+++ source/50_file.sh
@@ -45,3 +45,9 @@ alias c='pygmentize -O style=monokai -f console256 -g'
 
 # Fix NPM
 alias fixnpm='sudo chown -R $(whoami):admin $(npm config get prefix)'
+
+# Fast directory switching
+mkdir -p $DOTFILES/caches/z
+_Z_NO_PROMPT_COMMAND=1
+_Z_DATA=$DOTFILES/caches/z/z
+. $DOTFILES/vendor/z/z.sh
